b78f2962ccaecd24a8bfbdf0aa22d5bfbc7074f2
Angga Nurf <31128877+angganurf@users.noreply.github.com>
2025-05-22 18:08:16 +0700

    Update Cta.vue

--- app/components/home/Cta.vue
+++ app/components/home/Cta.vue
@@ -3,10 +3,10 @@
     class="flex flex-col items-center max-w-4xl p-8 mx-auto my-12 text-center bg-black rounded-lg md:px-20 md:py-20"
   >
     <h2 class="text-4xl tracking-tight text-white md:text-6xl">
-      {{ $t('home.cta.title') }}
+      
     </h2>
     <p class="mt-4 text-lg text-slate-400 md:text-xl">
-      {{ $t('home.cta.description') }}
+      
     </p>
   </div>
 </template>
